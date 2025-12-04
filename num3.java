77f13d0ddf71e218f87d4e7bb08c38a27788b3ec
dono-an,cerilo <araraocerilo@gmail.com>
2025-12-04 10:33:09 +0800

    Add files via upload
    december,4,2025

---
+++ num3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Write a description of class num3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class num3
+{
+    public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+    }
+    System.out.println();
+}}}
